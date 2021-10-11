91cbb0fca04b9da2e12c179660d8fbaef984d515
kauer3 <kaue0607@gmail.com>
2021-10-11 14:53:13 -0300

    First commit

---
+++ src/pages/Loan/index.js
@@ -0,0 +1,294 @@
+import React, { useState, useEffect } from "react";
+
+import api from "../../services/api";
+import moment from "moment";
+import "moment/locale/pt-br";
+
+import { Container, GlobalStyles, Form, Simulation } from "./styles";
+
+moment.locale("pt-br");
+
+const formatter = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+
+const formatDate = (date) => {
+  return moment.utc(date.toLocaleString()).format("DD/MM/YYYY");
+};
+
+export default function Loan() {
+  const [simulation, setSimulation] = useState({});
+  const [simulated, setSimulated] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [editData, setEditData] = useState({});
+  const [valueFocus, setValueFocus] = useState(false);
+  const [paybackFocus, setPaybackFocus] = useState(false);
+  const [submit, setSubmit] = useState(false);
+
+  const simulate = async (loan) => {
+    try {
+      const response = await api.get("/loan/simulate", {
+        params: {
+          cpf: loan.cpf,
+          uf: loan.uf,
+          birth: formatDate(loan.birth),
+          loan_amount: loan.loan_amount,
+          monthly_payback: loan.monthly_payback,
+        },
+      });
+      setSimulation(response.data);
+      setSimulated(true);
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  };
+
+  const saveLoan = async (loan) => {
+    try {
+      const response = await api.post("/loan", {
+        cpf: Number(loan.cpf),
+        uf: loan.uf,
+        birth: loan.birth,
+        loan_amount: loan.loan_amount,
+        monthly_payback: loan.monthly_payback,
+        monthly_fee: loan.monthly_fee,
+        total_fees: loan.total_fees,
+        months: loan.months,
+      });
+      setSaved(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Container>
+      <GlobalStyles simulated={simulated} />
+      {saved ? (
+        <h1 className="saved">Empréstimo efetivado com sucesso!</h1>
+      ) : (
+        <>
+          <h1 className="title">Simule e solicite o seu empréstimo.</h1>
+          <h5 className={`subTitle ${loading && "loading"}`}>
+            {loading
+              ? "Carregando..."
+              : "Preencha o formulário abaixo para simular"}
+          </h5>
+          <Form
+            simulated={simulated}
+            submit={submit}
+            onSubmit={(e) => {
+              e.preventDefault();
+              const sim = simulate(editData);
+              if (sim) {
+                setLoading(true);
+                setTimeout(() => {
+                  window.scroll({
+                    top: 800,
+                    behavior: "smooth",
+                  });
+                  setLoading(false);
+                }, 1000);
+              }
+            }}
+          >
+            <input
+              required
+              pattern="\d+"
+              type="number"
+              placeholder="CPF"
+              value={editData ? editData.cpf : ""}
+              onChange={(e) => {
+                setEditData({ ...editData, cpf: e.target.value });
+              }}
+            />
+            <select
+              required
+              placeholder="Categoria"
+              name="category"
+              id="category"
+              value={editData ? editData.uf : ""}
+              onChange={(e) => {
+                setEditData({ ...editData, uf: e.target.value });
+              }}
+            >
+              <option value="" disabled selected hidden>
+                UF
+              </option>
+              <option value="SP">SP</option>
+              <option value="MG">MG</option>
+              <option value="RJ">RJ</option>
+              <option value="ES">ES</option>
+            </select>
+            <input
+              required
+              type="date"
+              placeholder="DATA DE NASCIMENTO"
+              value={editData ? editData.birth : ""}
+              onChange={(e) => {
+                setEditData({ ...editData, birth: e.target.value });
+              }}
+            />
+            <input
+              required
+              type={valueFocus ? "number" : "text"}
+              min="50000"
+              placeholder="QUAL O VALOR DO EMPRÉSTIMO?"
+              value={
+                editData
+                  ? valueFocus
+                    ? editData.loan_amount
+                    : editData.loan_amount_formatted
+                  : ""
+              }
+              onChange={(e) => {
+                setEditData({ ...editData, loan_amount: e.target.value });
+              }}
+              onFocus={() => setValueFocus(true)}
+              onBlur={(e) => {
+                setValueFocus(false);
+                if (editData.loan_amount) {
+                  let value = editData.loan_amount;
+                  if (e.target.value < 50000) {
+                    value = 50000;
+                  }
+                  setEditData({
+                    ...editData,
+                    loan_amount: value,
+                    loan_amount_formatted: formatter.format(value),
+                  });
+                }
+              }}
+            />
+            <input
+              required
+              type={paybackFocus ? "number" : "text"}
+              min="0"
+              placeholder="QUAL VALOR DESEJA PAGAR POR MÊS?"
+              value={
+                editData
+                  ? paybackFocus
+                    ? editData.monthly_payback
+                    : editData.monthly_payback_formatted
+                  : ""
+              }
+              onChange={(e) => {
+                setEditData({ ...editData, monthly_payback: e.target.value });
+              }}
+              onFocus={() => setPaybackFocus(true)}
+              onBlur={(e) => {
+                setPaybackFocus(false);
+                if (editData.monthly_payback) {
+                  let value = editData.monthly_payback;
+                  if (e.target.value < 0) {
+                    value = 0;
+                  }
+                  setEditData({
+                    ...editData,
+                    monthly_payback: value,
+                    monthly_payback_formatted: formatter.format(value),
+                  });
+                }
+              }}
+            />
+            <input
+              type="submit"
+              value="Simular"
+              onClick={() => setSubmit(true)}
+            />
+          </Form>
+          {simulated && (
+            <h5
+              className={`subTitle simulation-subtitle ${
+                !simulation.approved && "error"
+              }`}
+            >
+              {simulation.approved
+                ? "Veja a simulação do seu empréstimo antes de efetivá-lo"
+                : simulation.message}
+            </h5>
+          )}
+          {simulation.approved && (
+            <Simulation>
+              <div className="first_section">
+                <div className="amount sub_section">
+                  <h5 className="name">VALOR REQUERIDO</h5>
+                  <h4 className="value">
+                    {formatter.format(simulation.loan_amount)}
+                  </h4>
+                </div>
+                <div className="fee sub_section">
+                  <h5 className="name">TAXA DE JUROS</h5>
+                  <h4 className="value">{simulation.monthly_fee}% ao mês</h4>
+                </div>
+                <div className="monthly_payback sub_section">
+                  <h5 className="name">VALOR DA PARCELA</h5>
+                  <h4 className="value">
+                    {formatter.format(simulation.monthly_payback)}
+                  </h4>
+                </div>
+                <div className="amount sub_section">
+                  <h5 className="name">TOTAL DE MESES PARA QUITAR</h5>
+                  <h4 className="value">{simulation.months}</h4>
+                </div>
+                <div className="amount sub_section">
+                  <h5 className="name">TOTAL DE JUROS</h5>
+                  <h4 className="value">
+                    {formatter.format(simulation.total_fees)}
+                  </h4>
+                </div>
+                <div className="amount sub_section">
+                  <h5 className="name">TOTAL A PAGAR</h5>
+                  <h4 className="value">
+                    {formatter.format(
+                      simulation.total_fees + Number(simulation.loan_amount)
+                    )}
+                  </h4>
+                </div>
+              </div>
+              <div className="second_section">
+                <h5 className="second_section_title">PROJEÇÃO DAS PARCELAS:</h5>
+                <table>
+                  <tr>
+                    <th>SALDO DEVEDOR</th>
+                    <th>JUROS</th>
+                    <th>SALDO DEVEDOR AJUSTADO</th>
+                    <th>VALOR DA PARCELA</th>
+                    <th>VENCIMENTO</th>
+                  </tr>
+                  {simulation.monthly_update.map((month) => {
+                    return (
+                      <tr>
+                        <td>{formatter.format(month.to_be_paid)}</td>
+                        <td>{formatter.format(month.current_fee)}</td>
+                        <td>{formatter.format(month.to_be_paid_with_fees)}</td>
+                        <td>{formatter.format(month.current_payback)}</td>
+                        <td>{month.expiration_date}</td>
+                      </tr>
+                    );
+                  })}
+                  <tr>
+                    <td>{formatter.format(0)}</td>
+                  </tr>
+                </table>
+                <hr />
+                <div className="buttonWrapper">
+                  <input
+                    type="button"
+                    className="submit"
+                    value="EFETIVAR O EMPRÉSTIMO"
+                    onClick={() => saveLoan(simulation)}
+                  />
+                </div>
+              </div>
+            </Simulation>
+          )}
+        </>
+      )}
+    </Container>
+  );
+}
---
+++ src/pages/Loan/styles.js
@@ -0,0 +1,259 @@
+import styled, {createGlobalStyle, keyframes} from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    background: #E5E5E5;
+    padding: ${props => props.simulated ? '20px 0 20px' : '5px 0 5px'} 0;
+    transition: ease-in-out .1s;
+  }
+`
+
+const Loading = keyframes`
+  0% {
+    transform: scale(1);
+    filter: invert(0%);
+  }
+  50% {
+    transform: scale(1.3, 1.1);
+    filter: invert(30%);
+  }
+  100% {
+    transform: scale(1);
+    filter: invert(0%);
+  }
+`
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  wrap: no-wrap;
+  align-items: center;
+
+  .saved {
+    position: absolute;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 40px;
+    font-weight: bold;
+    color: #444;
+    top: 35%;
+  }
+
+  .loading {
+    animation: ${Loading};
+    animation-duration: .5s;
+    animation-iteration-count: infinite;
+  }
+
+  .title {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 50px;
+    font-weight: 300;
+    color: #8F99A6;
+  }
+
+  .subTitle {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    color: #000;
+  }
+
+  .error {
+    color: #ff3434;
+  }
+
+  @media screen and (max-width: 830px) {
+    .title {
+      font-size: 6vw;
+    }
+
+    .subTitle, .saved {
+      font-size: 4vw;
+    }
+
+    .simulation-subtitle {
+      font-size: 3.3vw;
+    }
+  }
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  wrap: no-wrap;
+  gap: 15px;
+  align-items: center;
+  width: 64%;
+  padding: 70px 40px 40px;
+  background: #FFFFFF;
+  box-shadow: 0px 0px 10px #ECECEC;
+  border-radius: 5px;
+  margin-bottom:  ${props => props.simulated ? '50' : '0'}px;
+
+  input, select {
+    width: 100%;
+    height: 50px;
+    background: #FFFFFF;
+    box-sizing: border-box;
+    border-radius: 5px;
+    padding: 0 20px 0 20px;
+  }
+
+  input:not(last-child), select {
+    border: 1px solid ${props => props.submit ? '#ff3434' : '#D4D4D4'};
+    :required:valid {
+      border: 1px solid #D4D4D4;
+    }
+  }
+
+  select {
+    -moz-appearance:none;
+    -webkit-appearance:none;
+    appearance:none;
+    color: #737373;
+    :valid, option {
+      color: #000;
+    }
+  }
+
+  input[type="date"] {
+    color: #737373;
+    ::-webkit-calendar-picker-indicator {
+      filter: invert(45%);
+    }
+    :valid {
+      color: #000;
+      ::-webkit-calendar-picker-indicator {
+        filter: none;
+      }
+    }
+  }
+
+  input[type="submit"] {
+    background: #F3A126;
+    border: 1px solid #D4D4D4;
+    box-shadow: 0px 4px 4px rgba(135, 135, 135, 0.25);
+    font-family: 'Open Sans', sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    color: #FFF;
+    letter-spacing: 0.08em;
+    cursor: pointer;
+    transition: ease-out .3s;
+
+    :active {
+      filter: saturate(200%);
+      transform: scale(1.01);
+      transition: ease-out .1s;
+    }
+  }
+`
+
+export const Simulation = styled.div`
+  width: 64%;
+  padding: 10px 40px 40px;
+  background: #FFFFFF;
+  box-shadow: 0px 0px 10px #ECECEC;
+  border-radius: 5px;
+  font-family: 'Open Sans', sans-serif;
+
+  .first_section {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+    .sub_section {
+      width: 30%;
+      font-weight: 700;
+
+      .name {
+        font-size: 14px;
+        color: #737373;
+      }
+      .value {
+        font-size: 20px;
+        color: #333333;
+      }
+    }
+  }
+
+  .second_section {
+
+    .second_section_title {
+      font-weight: 700;
+      font-size: 14px;
+    }
+
+    table {
+      width: 100%;
+      text-align: left;
+      border-collapse: collapse;
+    }
+
+    td, th {
+      border-bottom: 1px solid #C4C4C4;
+      padding: 10px 0 10px 0;
+      font-size: 16px;
+      color: #333333;
+
+      :nth-child(2) {
+        width: 13%;
+      }
+    }
+
+    tr:last-child > td {
+      border: none;
+      padding: 10px 0 5px 0;
+    }
+
+    hr {
+      border-top: 1px solid #C4C4C4;
+      border-bottom: none;
+    }
+  }
+
+  .buttonWrapper {
+    text-align: center;
+
+    input[type="button"] {
+      width: 50%;
+      background: #21AE1E;
+      border: none;
+      box-shadow: 0px 4px 4px rgba(135, 135, 135, 0.25);
+      border-radius: 6px;
+      font-family: 'Open Sans', sans-serif;
+      font-size: 17px;
+      font-weight: bold;
+      color: #FFF;
+      letter-spacing: 0.08em;
+      text-align: center;
+      padding: 15px;
+      margin-top: 50px;
+      cursor: pointer;
+      transition: ease-out .3s;
+
+      :active {
+        filter: saturate(200%);
+        transform: scale(1.01);
+        transition: ease-out .1s;
+      }
+    }
+  }
+
+  @media screen and (max-width: 830px) {
+    .name, .value, td, th, input[type="button"] {
+      font-size: 2vw !important;
+    }
+
+    .second_section_title {
+      font-size: 3vw !important;
+    }
+
+    input[type="button"] {
+      font-size: 1.8vw !important;
+    }
+  }
+`
